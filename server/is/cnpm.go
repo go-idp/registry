2ae498d52174e09adbb6a7f827f3e0592fe3e9e9
Zero <tobewhatwewant@outlook.com>
2024-09-21 19:36:27 +0800

    fix: cnpm => egg-boilerplate-ts

--- server/is/cnpm.go
+++ server/is/cnpm.go
@@ -6,21 +6,44 @@ import (
 )
 
 // Cnpm check if the client is using cnpm
+//
+//	{
+//	  "Accept": [
+//	    "application/json"
+//	  ],
+//	  "Connection": [
+//	    "keep-alive"
+//	  ],
+//	  "User-Agent": [
+//	    "npminstall/6.5.1 node-urllib/3.0.0 Node.js/16.20.2 (OS X; arm64)"
+//	  ],
+//	  "X-Request-Id": [
+//	    "14987760a9f2/nTkGwZGh8l-000022"
+//	  ]
+//	}
+//
+//	{
+//		"Connection": [
+//			"close"
+//		],
+//		"Remoteip": [
+//			"222.71.122.104"
+//		],
+//		"User-Agent": [
+//			"node-urllib/3.27.0 Node.js/20.9.0 (OS X; arm64)"
+//		],
+//		"X-Request-Id": [
+//			"91e641ce1926/do9mLDeeMk-022983"
+//		]
+//	}
 func Cnpm(ctx *zoox.Context) (ok bool) {
-	// cnpm
-	//	{
-	//	  "Accept": [
-	//	    "application/json"
-	//	  ],
-	//	  "Connection": [
-	//	    "keep-alive"
-	//	  ],
-	//	  "User-Agent": [
-	//	    "npminstall/6.5.1 node-urllib/3.0.0 Node.js/16.20.2 (OS X; arm64)"
-	//	  ],
-	//	  "X-Request-Id": [
-	//	    "14987760a9f2/nTkGwZGh8l-000022"
-	//	  ]
-	//	}
-	return strings.Contains(ctx.UserAgent(), "npminstall/")
+	if ok := strings.Contains(ctx.UserAgent(), "npminstall/"); ok {
+		return true
+	}
+
+	if ok := strings.Contains(ctx.UserAgent(), "node-urllib/"); ok {
+		return true
+	}
+
+	return false
 }
